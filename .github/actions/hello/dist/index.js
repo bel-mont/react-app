525950963f9d669de94cdf41deffa6fdf6747650
bel-mont <cidhighwind85@hotmail.com>
2021-12-17 13:54:32 +0900

    feat(missing compile): compile

--- .github/actions/hello/dist/index.js
+++ .github/actions/hello/dist/index.js
@@ -927,15 +927,24 @@ const core = __webpack_require__(470);
 const github = __webpack_require__(469);
 
 try {
-  throw new Error('sample error');
+  // throw new Error('sample error');
+
+  core.debug('Debug message');
+  core.warning('Warning message');
+  core.error('Error message');
 
   const name = core.getInput('who-to-greet');
-  console.log(`Hello ${name}`);
+  const mySecret = 'my-secret';
+  core.setSecret(mySecret);
+  console.log(`Hello ${name}, ${mySecret}`);
 
   const time = new Date();
   core.setOutput('time', time.toTimeString());
 
+  core.startGroup('Logging github object');
   console.log(JSON.stringify(github, null, '\t'));
+  core.endGroup();
+  core.exportVariable('HELLO', 'hello var text');
 } catch (error) {
   core.setFailed('panic!', error.message);
 }
