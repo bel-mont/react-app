2334f08022e2e560e3b2db92c14195b25ed46012
bel-mont <cidhighwind85@hotmail.com>
2021-12-17 13:54:10 +0900

    feat(more custom action stuff): actions

--- .github/actions/hello/index.js
+++ .github/actions/hello/index.js
@@ -2,15 +2,24 @@ const core = require('@actions/core');
 const github = require('@actions/github');
 
 try {
-  throw new Error('sample error');
+  // throw new Error('sample error');
+
+  core.debug('Debug message');
+  core.warning('Warning message');
+  core.error('Error message');
 
   const name = core.getInput('who-to-greet');
-  console.log(`Hello ${name}`);
+  const mySecret = 'my-secret';
+  core.setSecret(mySecret);
+  console.log(`Hello ${name}, ${mySecret}`);
 
   const time = new Date();
   core.setOutput('time', time.toTimeString());
 
+  core.startGroup('Logging github object');
   console.log(JSON.stringify(github, null, '\t'));
+  core.endGroup();
+  core.exportVariable('HELLO', 'hello var text');
 } catch (error) {
   core.setFailed('panic!', error.message);
 }
